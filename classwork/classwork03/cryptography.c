91c4e3720b8e6c75de69283589236813f07beea7
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2023-09-16 21:09:12 -0700

    Started Creating Program

--- classwork/classwork03/cryptography.c
+++ classwork/classwork03/cryptography.c
@@ -8,6 +8,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_VALUE 5999
+
 int main(int argc, char *argv[])
 {
+    // Asks users for their input that totals of 50 characters.
+    char input[50];
+    int index;
+    int value;
+    char *morse[".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+                "..", ".---", "-.-", ".-..", "--", "-.", "--", "-.", "---",
+                ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+                "-.--", "--..", ".----", "..---", "...--", "....-", ".....",
+                "-....", "--...", "---..", "----.", "-----"];
+
+    if (argv == 1)
+    {
+        printf("Please try again and type more words mister.");
+        exit(0)
+    }
+
+    value = atoi(arg[1]);
+
+    printf("What's you name?");
+    scanf("%s", input);
+
+    printf("\n\tHello, %s! It's nice to meet you? How's life treating you?\n", input);
+    exit(0);
 }
