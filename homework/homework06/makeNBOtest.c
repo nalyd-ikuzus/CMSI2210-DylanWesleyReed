24d644128a83dfc0da627a01a531e16232cb1661
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2023-11-28 00:45:06 -0800

    Added whichEndIsUp to access from makeNBO

---
+++ homework/homework06/makeNBOtest.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+int makeNBO(int);
+
+int main()
+{
+    /*
+        Program that tests the correctness
+        of makeNBO.c.
+    */
+
+    // Test for making sure that the signed numbers switch properly.
+    assert(makeNBO(240) == -268435456);
+    assert(makeNBO(-268435456) == 240);
+
+    // Test to see for palindromatic bytes, the number does not change.
+    assert(makeNBO(-572662307) == -572662307);
+    assert(makeNBO(1236447817) == 1236447817);
+
+    // Test to see if the middle bytes switch
+    assert(makeNBO(8952064) == 10061824);
+    assert(makeNBO(10061824) == 8952064);
+
+    return 0;
+}
--- homework/homework06/whichEndIsUp.c
+++ homework/homework06/whichEndIsUp.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
+int whichEndIsUp();
+
+int whichEndIsUp()
 {
+    /*
+        Checks the computer's endian.
+
+        Returns:
+            int:
+                Returns 1 if computer is Little Endian.
+                Returns 0 if computer is Big Endian.
+    */
     int num = 1;
     char *cpointer = (char *)&num;
     if (cpointer)
