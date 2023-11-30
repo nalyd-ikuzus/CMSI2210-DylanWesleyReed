58e47bb8dcbaeefdb380d17bec00ca68dc7fd00e
dylansuzuki <dylans2022@headroyce.org>
2023-11-29 17:34:32 -0800

    classwork 14 complete

---
+++ classwork/classwork14/client.c
@@ -0,0 +1,45 @@
+   #include <stdio.h>
+   #include <sys/socket.h>
+   #include <arpa/inet.h>
+   #include <unistd.h>
+   #include <string.h>
+
+   #define PORT 8080
+
+   int main( int argc, char const *argv[] ) {
+       int sock = 0, valread;
+       struct sockaddr_in serv_addr;
+       char *hello = "Hello from client";
+       char buffer[1024] = {0};
+       char input[1024];
+       if( (sock = socket( AF_INET, SOCK_STREAM, 0 )) < 0 ) {
+           printf( "\n Socket creation error \n" );
+           return -1;
+       }
+       serv_addr.sin_family = AF_INET;
+       serv_addr.sin_port = htons(PORT);
+       if( inet_pton( AF_INET, "127.0.0.1", &serv_addr.sin_addr )<= 0 ) {
+           printf( "\nInvalid address or Address not supported \n" );
+           return -1;
+       }
+       if( connect( sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0 ) {
+           printf( "\nConnection Failed \n" );
+           return -1;
+       }
+       send( sock, hello, strlen(hello), 0 );
+       printf( "   client says: Hello message sent from client\n" );
+       valread = read( sock, buffer, 1024 );
+       printf( "   client says: server sent: %s\n", buffer );
+       while(1){
+           input[0] = '\0';
+           printf("\nEnter your message to the server (type quit to quit): ");
+           fgets(input, 1024, stdin);
+           send( sock, input, strlen(input), 0 );
+           printf("\nSent message");
+           if( strcmp(input, "quit\n") == 0 ){
+               break;
+           }
+
+       }
+       return 0;
+   }
